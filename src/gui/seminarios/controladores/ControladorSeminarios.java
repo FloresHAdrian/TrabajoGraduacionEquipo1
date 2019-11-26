881602cf34a7340042d11049e5b00118811bdd4f
Flores Hector Adrian <floreshadrian@gmail.com>
2019-11-26 16:45:49 -0300

    Creacion rudimentaria de la VentanaSeminarios y controlador Cambio de la version de NeatBeans del proyecto

---
+++ src/gui/seminarios/controladores/ControladorSeminarios.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.seminarios.controladores;
+
+import gui.interfaces.IControladorSeminarios;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowEvent;
+
+/**
+ *
+ * @author Adrian
+ */
+public class ControladorSeminarios implements IControladorSeminarios{
+
+    @Override
+    public void btnNuevoSeminarioClic(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void btnModificarSeminarioClic(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void btnVolverClic(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ventanaGanaFoco(WindowEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
