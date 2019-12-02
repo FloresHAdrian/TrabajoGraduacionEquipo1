7bf9f97187de6271436e327dbe49f4c4b999b85b
Flores Hector Adrian <floreshadrian@gmail.com>
2019-12-02 13:24:53 -0300

    otros cambios

--- src/gui/seminarios/modelos/GestorSeminarios.java
+++ src/gui/seminarios/modelos/GestorSeminarios.java
@@ -166,10 +166,11 @@ public class GestorSeminarios implements IGestorSeminarios{
                                 String observaciones = vector[i++];//guardamos observaciones en el tercer vector
                                 trabajo.agregarSeminario(new Seminario(fechaExposicion, NotaAprobacion.APROBADO_CO, observaciones));
                             } else if (nota.equalsIgnoreCase("Aprobado S/O")) {
-                                String observaciones = vector[i++];
-                                if (observaciones.equalsIgnoreCase(" ")) {
-                                    observaciones = null;//si es desaprobado mando null a las observaciones
-                                }
+                                i++;
+                                String observaciones = null;
+//                                if (observaciones.equalsIgnoreCase(" ")) {
+//                                    observaciones = null;//si es desaprobado mando null a las observaciones
+//                                }
                                 trabajo.agregarSeminario(new Seminario(fechaExposicion, NotaAprobacion.APROBADO_SO, observaciones));
                             } else if (nota.equalsIgnoreCase("Desaprobado")) {
                                 String observaciones = vector[i++];//guardamos observaciones en el tercer vector
